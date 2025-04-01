aeab00d03360a3a0f0bf13d82115c5186676b746
Приказчиков Иван <inprikazchikov@bk.ru>
2025-04-01 22:39:56 +0300

    мелкие правки build v1.2.1

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ if (container) {
 
   root.render(
     <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={'/Marusya-react'}>
         <App />
       </BrowserRouter>
     </StrictMode>,
